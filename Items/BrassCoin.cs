9e5d44b0844d2241f23b51d3c914a075cf1170d2
Flash Kirby <flashkirby@gmail.com>
2017-01-21 11:47:35 +0000

    Add quest items

---
+++ Items/BrassCoin.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ExpeditionsContent.Items
+{
+    public class BrassCoin : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Vintage Coin";
+            item.toolTip = "'An old coin made of brass, it has no value'";
+            item.maxStack = 100;
+            item.width = 16;
+            item.height = 18;
+            item.rare = -1;
+        }
+    }
+}
---
+++ Items/HeartLocket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExpeditionsContent.Items
+{
+    public class HeartLocket : ModItem
+    {
+        public override bool Autoload(ref string name, ref string texture, IList<EquipType> equips)
+        {
+            equips.Add(EquipType.Neck);
+            return true;
+        }
+
+        public override void SetDefaults()
+        {
+            item.name = "Heart Locket";
+            item.toolTip = "Temporarily increases maximum life by 20";
+            item.width = 22;
+            item.height = 30;
+            item.accessory = true;
+            item.rare = 1;
+            item.value = Item.buyPrice(0, 1, 0, 0);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.statLifeMax2 += 20;
+        }
+    }
+}
