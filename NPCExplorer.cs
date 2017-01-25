7d68a36a68d73eed0dcf5d8b15d5833a2ed009e6
Flash Kirby <flashkirby@gmail.com>
2017-01-25 00:41:45 +0000

    Make expedition board dialogue much more common until 1st expedition is cleared

--- NPCExplorer.cs
+++ NPCExplorer.cs
@@ -7,17 +7,22 @@ using Terraria.Map;
 using Terraria.ID;
 using Terraria.ModLoader;
 
+using Expeditions;
+
 namespace ExpeditionsContent
 {
     public class NPCExplorer : GlobalNPC
     {
         public override void GetChat(NPC npc, ref string chat)
         {
-            if (npc.type == NPCID.Guide)
+            if (npc.type == NPCID.Guide &&
+                !Main.hardMode &&
+                !API.FindExpedition<Quests.Core.AAWelcomeQuest>(mod).completed
+                )
             {
                 if (Main.dayTime)
                 {
-                    switch(Main.rand.Next(4))
+                    switch(Main.rand.Next(2))
                     {
                         case 0:
                             chat = "You can craft an expedition board at a workbench using wood. I will post additional advice on there any time you need help. ";
