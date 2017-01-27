33a044ee63049f2eb91f74addfc5d803128a24e5
Flash Kirby <flashkirby@gmail.com>
2017-01-27 01:45:02 +0000

    Add flavour text

--- Items/HeartLocket.cs
+++ Items/HeartLocket.cs
@@ -16,6 +16,7 @@ namespace ExpeditionsContent.Items
         {
             item.name = "Heart Locket";
             item.toolTip = "Temporarily increases maximum life by 20";
+            item.toolTip2 = "'Fashionable, and stylish'";
             item.width = 22;
             item.height = 30;
             item.accessory = true;
