fe930ad3dc0584b192032deb0efa4fea4e4a9998
Flash Kirby <flashkirby@gmail.com>
2017-02-12 19:41:21 +0000

    Update camera upgrade quest

--- Quests/Clerk/ProCamSkill.cs
+++ Quests/Clerk/ProCamSkill.cs
@@ -16,9 +16,11 @@ namespace ExpeditionsContent.Quests.Clerk
             expedition.ctgCollect = true;
             expedition.ctgExplore = true;
 
-            expedition.conditionDescription1 = "Tim";
+            expedition.conditionDescription1 = "Ice Golem";
             expedition.conditionDescription2 = "Moth";
             expedition.conditionDescription3 = "Truffle Worm";
+            expedition.conditionCountedMax = 3;
+            expedition.conditionDescriptionCountable = "Take photos of listed creatures";
         }
         public override void AddItemsOnLoad()
         {
@@ -29,36 +31,43 @@ namespace ExpeditionsContent.Quests.Clerk
         {
             return "Do you think your photography skills are a cut above the rest? Well first you'll have to prove yourself - try getting shots of these rare creatures. If you can do that for me, I will gladly provide you with more professional equipment!  ";
         }
+        #region Photo Bools
+        public static bool IceGolem
+        { get { return PhotoManager.PhotoOfNPC[NPCID.IceGolem]; } }
+        public static bool Moth
+        { get { return PhotoManager.PhotoOfNPC[NPCID.Moth]; } }
+        public static bool TruffleWorm
+        { get { return PhotoManager.PhotoOfNPC[NPCID.TruffleWorm] || PhotoManager.PhotoOfNPC[NPCID.TruffleWormDigger]; } }
+        #endregion
 
         public override bool CheckPrerequisites(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
         {
             return PlayerExplorer.HoldingCamera(mod) && Main.hardMode;
         }
-        public readonly int[] photosToMatch = new int[]
-        {
-            NPCID.Tim,
-            NPCID.Moth,
-            NPCID.TruffleWorm,
-            NPCID.TruffleWormDigger
-        };
 
         public override void CheckConditionCountable(Player player, ref int count, int max)
         {
-            count = PhotoManager.CountUniquePhotosInInventory(photosToMatch);
+            count = 0;
+            if (IceGolem) count++;
+            if (Moth) count++;
+            if (TruffleWorm) count++;
         }
 
         public override bool CheckConditions(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
         {
-            cond1 = PhotoManager.PhotoOfNPC[NPCID.Tim];
-            cond2 = PhotoManager.PhotoOfNPC[NPCID.Moth];
-            cond3 = PhotoManager.PhotoOfNPC[NPCID.TruffleWorm]
-                || PhotoManager.PhotoOfNPC[NPCID.TruffleWormDigger];
+            cond1 = IceGolem;
+            cond2 = Moth;
+            cond3 = TruffleWorm;
             return cond1 && cond2 && cond3;
         }
 
         public override void PreCompleteExpedition(List<Item> rewards, List<Item> deliveredItems)
         {
-            PhotoManager.ConsumePhotos(photosToMatch);
+            PhotoManager.ConsumePhoto(NPCID.IceGolem);
+            PhotoManager.ConsumePhoto(NPCID.Moth);
+
+            if (!PhotoManager.ConsumePhoto(NPCID.TruffleWormDigger))
+            { PhotoManager.ConsumePhoto(NPCID.TruffleWorm); }
         }
     }
 }
