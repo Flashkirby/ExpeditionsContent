fc82c955fa1e09234edf4c177bc508cddf320bb7
Flash Kirby <flashkirby@gmail.com>
2017-02-13 01:59:52 +0000

    Add bee quest

---
+++ Quests/Clerk/AlbumAntlions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Expeditions;
+using System.Collections.Generic;
+
+namespace ExpeditionsContent.Quests.Clerk
+{
+    class AlbumAntlions : ModExpedition
+    {
+        public override void SetDefaults()
+        {
+            expedition.name = "Snap! An Antlion's Life";
+            SetNPCHead(ExpeditionC.NPCIDClerk);
+            expedition.difficulty = 1;
+            expedition.ctgCollect = true;
+            expedition.ctgExplore = true;
+            expedition.repeatable = true;
+
+            expedition.conditionDescription1 = "Antlion";
+            expedition.conditionDescription2 = "Antlion Charger";
+            expedition.conditionDescription3 = "Antlion Swarmer";
+            expedition.conditionCountedMax = 3;
+            expedition.conditionDescriptionCountable = "Take photos of listed creatures";
+        }
+        public override void AddItemsOnLoad()
+        {
+            AddRewardItem(API.ItemIDExpeditionCoupon);
+            AddRewardItem(mod.ItemType<Items.Albums.AlbumAntlion>());
+        }
+        public override string Description(bool complete)
+        {
+            return "Here's a task for you - have you looked at the various insects residing in that giant pit in the desert? They're all antlions of some kind, but perhaps if you take some photos I can do some homework into their lifecycle. ";
+        }
+        #region Photo Bools
+        public static bool Antlion
+        { get { return PhotoManager.PhotoOfNPC[NPCID.Antlion]; } }
+        public static bool Charger
+        { get { return PhotoManager.PhotoOfNPC[NPCID.WalkingAntlion]; } }
+        public static bool Swarmer
+        { get { return PhotoManager.PhotoOfNPC[NPCID.FlyingAntlion]; } }
+        #endregion
+
+        public override bool CheckPrerequisites(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
+        {
+            return PlayerExplorer.HoldingCamera(mod)
+                || expedition.conditionCounted > 0;
+        }
+
+        public override void CheckConditionCountable(Player player, ref int count, int max)
+        {
+            count = 0;
+            if (Antlion) count++;
+            if (Charger) count++;
+            if (Swarmer) count++;
+        }
+
+        public override bool CheckConditions(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
+        {
+            cond1 = Antlion;
+            cond2 = Charger;
+            cond3 = Swarmer;
+            return cond1 && cond2 && cond3;
+        }
+
+        public override void PreCompleteExpedition(List<Item> rewards, List<Item> deliveredItems)
+        {
+            PhotoManager.ConsumePhoto(NPCID.Antlion);
+            PhotoManager.ConsumePhoto(NPCID.WalkingAntlion);
+            PhotoManager.ConsumePhoto(NPCID.FlyingAntlion);
+
+            // Only reward the coupon once!
+            if (expedition.completed)
+            { rewards[0] = new Item(); }
+        }
+    }
+}
---
+++ Quests/Clerk/AlbumBEES.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Expeditions;
+using System.Collections.Generic;
+
+namespace ExpeditionsContent.Quests.Clerk
+{
+    class AlbumBEES : ModExpedition
+    {
+        public override void SetDefaults()
+        {
+            expedition.name = "Snap! Photo Buzz";
+            SetNPCHead(ExpeditionC.NPCIDClerk);
+            expedition.difficulty = 1;
+            expedition.ctgCollect = true;
+            expedition.ctgExplore = true;
+            expedition.repeatable = true;
+
+            expedition.conditionDescription1 = "Small Bee";
+            expedition.conditionDescription2 = "Large Bee";
+            expedition.conditionDescription3 = "Hornet";
+            expedition.conditionCountedMax = 3;
+            expedition.conditionDescriptionCountable = "Take photos of listed creatures";
+        }
+        public override void AddItemsOnLoad()
+        {
+            AddRewardItem(API.ItemIDExpeditionCoupon);
+            AddRewardItem(mod.ItemType < Items.Albums.AlbumBee>());
+        }
+        public override string Description(bool complete)
+        {
+            return "The jungle has a healthy population of bees and... big bees? They're called hornets but I'm not too sure. You may need to disturb a couple of beehives to get the photos we're looking for... You'll be fine, trust me! ";
+        }
+        #region Photo Bools
+        public static bool Bee1
+        { get { return PhotoManager.PhotoOfNPC[NPCID.BeeSmall]; } }
+        public static bool Bee2
+        { get { return PhotoManager.PhotoOfNPC[NPCID.Bee]; } }
+        public static bool Hornet
+        { get { return PhotoManager.PhotoOfNPC[NPCID.Hornet]; } }
+        #endregion
+
+        public override bool CheckPrerequisites(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
+        {
+            return PlayerExplorer.HoldingCamera(mod)
+                || expedition.conditionCounted > 0;
+        }
+
+        public override void CheckConditionCountable(Player player, ref int count, int max)
+        {
+            count = 0;
+            if (Bee1) count++;
+            if (Bee2) count++;
+            if (Hornet) count++;
+        }
+
+        public override bool CheckConditions(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
+        {
+            cond1 = Bee1;
+            cond2 = Bee2;
+            cond3 = Hornet;
+            return cond1 && cond2 && cond3;
+        }
+
+        public override void PreCompleteExpedition(List<Item> rewards, List<Item> deliveredItems)
+        {
+            PhotoManager.ConsumePhoto(NPCID.BeeSmall);
+            PhotoManager.ConsumePhoto(NPCID.Bee);
+            PhotoManager.ConsumePhoto(NPCID.Hornet);
+
+            // Only reward the coupon once!
+            if (expedition.completed)
+            { rewards[0] = new Item(); }
+        }
+    }
+}
