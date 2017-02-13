00c8c6a161c32c49718fd055e0357e7346d5e169
Flash Kirby <flashkirby@gmail.com>
2017-02-13 01:24:27 +0000

    Add second slime quest

---
+++ Quests/Clerk/AlbumSlimes2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Expeditions;
+using System.Collections.Generic;
+
+namespace ExpeditionsContent.Quests.Clerk
+{
+    class AlbumSlimes2 : ModExpedition
+    {
+        public override void SetDefaults()
+        {
+            expedition.name = "Snap! Dark and Slimy";
+            SetNPCHead(ExpeditionC.NPCIDClerk);
+            expedition.difficulty = 2;
+            expedition.ctgCollect = true;
+            expedition.ctgExplore = true;
+            expedition.repeatable = true;
+
+            expedition.conditionDescription1 = "Umbrella Slime";
+            expedition.conditionDescription2 = "Dungeon Slime";
+            expedition.conditionDescription3 = "Lava Slime";
+            expedition.conditionCountedMax = 3;
+            expedition.conditionDescriptionCountable = "Take photos of listed creatures";
+        }
+        public override void AddItemsOnLoad()
+        {
+            AddDeliverable(mod.ItemType<Items.Albums.AlbumSlimes>());
+
+            AddRewardItem(API.ItemIDExpeditionCoupon);
+            AddRewardItem(mod.ItemType<Items.Albums.AlbumSlimes2>());
+        }
+        public override string Description(bool complete)
+        {
+            return "There are plenty of diverse and interesting slimes out there, inhabiting all kinds of different places, and adapting to all kinds of environments! See if you can snap some good examples of these kindss of slimes. ";
+        }
+        #region Photo Bools
+        public static bool Umbrella
+        { get { return PhotoManager.PhotoOfNPC[NPCID.UmbrellaSlime]; } }
+        public static bool Dungeon
+        { get { return PhotoManager.PhotoOfNPC[NPCID.DungeonSlime]; } }
+        public static bool Lava
+        { get { return PhotoManager.PhotoOfNPC[NPCID.LavaSlime]; } }
+        #endregion
+
+        public override bool CheckPrerequisites(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
+        {
+            return (PlayerExplorer.HoldingCamera(mod) && API.FindExpedition<AlbumSlimes>(mod).completed)
+                || expedition.conditionCounted > 0;
+        }
+
+        public override void CheckConditionCountable(Player player, ref int count, int max)
+        {
+            count = 0;
+            if (Umbrella) count++;
+            if (Dungeon) count++;
+            if (Lava) count++;
+        }
+
+        public override bool CheckConditions(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
+        {
+            cond1 = Umbrella;
+            cond2 = Dungeon;
+            cond3 = Lava;
+            return cond1 && cond2 && cond3;
+        }
+
+        public override void PreCompleteExpedition(List<Item> rewards, List<Item> deliveredItems)
+        {
+            PhotoManager.ConsumePhoto(NPCID.UmbrellaSlime);
+            PhotoManager.ConsumePhoto(NPCID.DungeonSlime);
+            PhotoManager.ConsumePhoto(NPCID.LavaSlime);
+
+            // Only reward the coupon once!
+            if (expedition.completed)
+            { rewards[0] = new Item(); }
+        }
+    }
+}
