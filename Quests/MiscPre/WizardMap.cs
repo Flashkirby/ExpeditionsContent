2959dcbb0cf7ea33fbe61685936e7c2df32fdab2
Flash Kirby <flashkirby@gmail.com>
2017-02-18 19:41:53 +0000

    Change wizard quest to check hardmode just in case

--- Quests/MiscPre/WizardMap.cs
+++ Quests/MiscPre/WizardMap.cs
@@ -11,7 +11,7 @@ namespace ExpeditionsContent.Quests.MiscPre
         public override void SetDefaults()
         {
             expedition.name = "Enchanted Grotto";
-            SetNPCHead(NPCID.Wizard);
+            SetNPCHead(NPCID.Wizard, true);
             expedition.difficulty = 4;
             expedition.ctgCollect = true;
         }
@@ -30,6 +30,11 @@ namespace ExpeditionsContent.Quests.MiscPre
                 return "Ah, I do believe I remember where it was. Or they were. Behold, I present you this magically enchanted map - it may or may not eat you! Oh, you'd rather have this regular old magic map? Ok. ";
             }
             return "I came across this wonderful shrine once, but I can't remember where. It had a magical sword in it... maybe? Or was it a rock? You know, a memory boost might do the trick! Fetch me these ingredients, perhaps I may recall something? ";
+        }
+
+        public override bool CheckPrerequisites(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
+        {
+            return Main.hardMode = true;
         }
     }
 }
