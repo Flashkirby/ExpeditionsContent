49e7bb67da0a536af291a2e4e45f30c9c88c7980
Flash Kirby <flashkirby@gmail.com>
2017-03-15 22:30:44 +0000

    Add moonstone

---
+++ Items/Moonstone.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace ExpeditionsContent.Items
+{
+    public class Moonstone : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Moon Gem";
+            item.toolTip = "'Sparkles like the night sky'";
+            item.width = 22;
+            item.height = 22;
+            item.maxStack = 99;
+            item.rare = 1;
+            item.value = Item.buyPrice(0, 1, 0, 0);
+        }
+
+        public override void Update(ref float gravity, ref float maxFallSpeed)
+        {
+            int lightMult = Main.moonPhase - 4;
+            lightMult = System.Math.Abs(lightMult);
+            Lighting.AddLight(item.Center, 
+                0.66f - 0.02f * lightMult, 
+                0.62f + 0.05f * lightMult, 0.9f);
+        }
+
+        public override DrawAnimation GetAnimation()
+        {
+            return new DrawAnimationMoonstone(8);
+        }
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            Main.itemFrame[whoAmI] = Main.moonPhase;
+            Main.itemFrameCounter[whoAmI] = 0;
+            return true;
+        }
+    }
+
+    internal class DrawAnimationMoonstone : DrawAnimation
+    {
+        public DrawAnimationMoonstone(int frameCount)
+        {
+            this.FrameCount = frameCount;
+            this.TicksPerFrame = 2;
+        }
+
+        public override void Update()
+        {
+            this.Frame = Main.moonPhase;
+            this.FrameCounter = 0;
+        }
+
+        public override Rectangle GetFrame(Texture2D texture)
+        {
+            return texture.Frame(1, this.FrameCount, 0, this.Frame);
+        }
+    }
+}
