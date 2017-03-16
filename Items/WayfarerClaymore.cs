fa4d83dc91e2edfee5540765def50263bd501f3f
Flash Kirby <flashkirby@gmail.com>
2017-03-16 01:36:35 +0000

    Add claymore and aubber

---
+++ Items/WayfarerClaymore.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExpeditionsContent.Items
+{
+    public class WayfarerClaymore : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.BladeofGrass);
+            item.name = "Wayfarer's Claymore";
+            item.width = 32;
+            item.height = 36;
+            item.scale = 1.15f;
+
+            item.damage += 3; //poisoned is 2/s for 7s on 25% change so average 3.5
+            item.useAnimation -= 3;
+            item.knockBack = 7f;
+            item.autoReuse = true;
+
+            item.rare = 2;
+            item.value = Item.buyPrice(0, 0, 60, 0);
+        }
+    }
+}
---
+++ Items/WayfarerSubArm.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExpeditionsContent.Items
+{
+    public class WayfarerSubArm : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.Minishark);
+            item.name = "Wayfarer's Subber";
+            item.toolTip = "50% chance not to consume ammo";
+            item.toolTip2 = "'Spray and pray'";
+            item.width = 42;
+            item.height = 24;
+
+            item.UseSound = new LegacySoundStyle(42, 194);
+            item.damage = 4;
+            item.knockBack = 0.5f;
+            item.useAnimation = 5;
+            item.useTime = 5;
+            item.shootSpeed += 2f;
+
+            item.value = Item.sellPrice(0, 1, 0, 0);
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            speedX += Main.rand.NextFloatDirection() * 2f;
+            speedY += Main.rand.NextFloatDirection() * 2f;
+            return true;
+        }
+        public override bool ConsumeAmmo(Player player)
+        {
+            return Main.rand.NextBool();
+        }
+
+        public override void HoldItem(Player player)
+        {
+            if (player.itemAnimation == player.itemAnimationMax - 1)
+            {
+                player.itemRotation += Main.rand.NextFloatDirection() * 0.13f;
+            }
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-10, 3);
+        }
+    }
+}
