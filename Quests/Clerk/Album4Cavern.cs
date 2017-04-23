a1445185946ac765b50ea033ce21eb7293ed14e4
Flash Kirby <flashkirby@gmail.com>
2017-04-23 13:17:35 +0100

    Update future albums to use new tier categorisation

--- Quests/Clerk/Album4Cavern.cs
+++ Quests/Clerk/Album4Cavern.cs
@@ -6,36 +6,42 @@ using System.Collections.Generic;
 
 namespace ExpeditionsContent.Quests.Clerk
 {
-    class AlbumMushi2 : ModExpedition
+    class Album4Cavern : ModExpedition
     {
         public override void SetDefaults()
         {
-            expedition.name = "Snap! Freakish Fungus";
+            expedition.name = "Snap! Cave Life, Again";
             SetNPCHead(ExpeditionC.NPCIDClerk);
-            expedition.difficulty = 2;
+            expedition.difficulty = 4;
             expedition.ctgCollect = true;
             expedition.ctgExplore = true;
             expedition.repeatable = true;
 
-            expedition.conditionDescription1 = "Anomura Fungus";
-            expedition.conditionDescription2 = "Giant Fungi Bulb";
-            expedition.conditionDescription3 = "Fungo Fish";
+            expedition.conditionDescription1 = "Giant Bat";
+            expedition.conditionDescription2 = "Digger";
+            expedition.conditionDescription3 = "Black Recluse";
             expedition.conditionCountedMax = 3;
             expedition.conditionDescriptionCountable = "Take photos of listed creatures";
         }
         public override void AddItemsOnLoad()
         {
             AddRewardItem(API.ItemIDExpeditionCoupon, 1, true);
-            AddRewardItem(mod.ItemType<Items.Albums.AlbumMushroom2>());
+            AddRewardItem(mod.ItemType<Items.Albums.AlbumCavern2>());
         }
         public override string Description(bool complete)
         {
-            return "Given how everything else has changed, what say you check out a mushroom biome, see what crazy new things you might find there.  ";
+            return "There are a lot of powerful new enemies wandering about, but there should be some familiar creatures in the caverns for you to look out for. Bigger and tougher sure, but otherwise they're the same, I think. ";
         }
         #region Photo Bools
-        public static PhotoManager af = new PhotoManager(NPCID.AnomuraFungus);
-        public static PhotoManager gfb = new PhotoManager(NPCID.GiantFungiBulb);
-        public static PhotoManager ff = new PhotoManager(NPCID.FungoFish);
+        public static bool CBat
+        { get { return PhotoManager.PhotoOfNPC[NPCID.GiantBat]; } }
+        public static bool Worm
+        { get { return 
+                    PhotoManager.PhotoOfNPC[NPCID.DiggerHead] ||
+                    PhotoManager.PhotoOfNPC[NPCID.DiggerBody] ||
+                    PhotoManager.PhotoOfNPC[NPCID.DiggerTail]; } }
+        public static bool WallCreeper
+        { get { return PhotoManager.PhotoOfNPC[NPCID.BlackRecluse] || PhotoManager.PhotoOfNPC[NPCID.BlackRecluseWall]; } }
         #endregion
 
         public override bool CheckPrerequisites(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
@@ -48,24 +54,29 @@ namespace ExpeditionsContent.Quests.Clerk
         public override void CheckConditionCountable(Player player, ref int count, int max)
         {
             count = 0;
-            if (af.checkValid()) count++;
-            if (gfb.checkValid()) count++;
-            if (ff.checkValid()) count++;
+            if (CBat) count++;
+            if (Worm) count++;
+            if (WallCreeper) count++;
         }
 
         public override bool CheckConditions(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
         {
-            cond1 = af.checkValid();
-            cond2 = gfb.checkValid();
-            cond3 = ff.checkValid();
+            cond1 = CBat;
+            cond2 = Worm;
+            cond3 = WallCreeper;
             return cond1 && cond2 && cond3;
         }
 
         public override void PreCompleteExpedition(List<Item> rewards, List<Item> deliveredItems)
         {
-            af.consumePhoto();
-            gfb.consumePhoto();
-            ff.consumePhoto();
+            PhotoManager.ConsumePhoto(NPCID.GiantBat);
+            if (!PhotoManager.ConsumePhoto(NPCID.DiggerHead))
+            {
+                if (!PhotoManager.ConsumePhoto(NPCID.DiggerBody))
+                { PhotoManager.ConsumePhoto(NPCID.DiggerTail); }
+            }
+            if (!PhotoManager.ConsumePhoto(NPCID.BlackRecluseWall))
+            { PhotoManager.ConsumePhoto(NPCID.BlackRecluse); }
 
             // Only reward the coupon once!
             if (expedition.completed)
--- Quests/Clerk/Album4Mushi.cs
+++ Quests/Clerk/Album4Mushi.cs
@@ -6,42 +6,36 @@ using System.Collections.Generic;
 
 namespace ExpeditionsContent.Quests.Clerk
 {
-    class AlbumCavern2 : ModExpedition
+    class Album4Mushi : ModExpedition
     {
         public override void SetDefaults()
         {
-            expedition.name = "Snap! Cave Life, Again";
+            expedition.name = "Snap! Freakish Fungus";
             SetNPCHead(ExpeditionC.NPCIDClerk);
-            expedition.difficulty = 4;
+            expedition.difficulty = 2;
             expedition.ctgCollect = true;
             expedition.ctgExplore = true;
             expedition.repeatable = true;
 
-            expedition.conditionDescription1 = "Giant Bat";
-            expedition.conditionDescription2 = "Digger";
-            expedition.conditionDescription3 = "Black Recluse";
+            expedition.conditionDescription1 = "Anomura Fungus";
+            expedition.conditionDescription2 = "Giant Fungi Bulb";
+            expedition.conditionDescription3 = "Fungo Fish";
             expedition.conditionCountedMax = 3;
             expedition.conditionDescriptionCountable = "Take photos of listed creatures";
         }
         public override void AddItemsOnLoad()
         {
             AddRewardItem(API.ItemIDExpeditionCoupon, 1, true);
-            AddRewardItem(mod.ItemType<Items.Albums.AlbumCavern2>());
+            AddRewardItem(mod.ItemType<Items.Albums.AlbumMushroom2>());
         }
         public override string Description(bool complete)
         {
-            return "There are a lot of powerful new enemies wandering about, but there should be some familiar creatures in the caverns for you to look out for. Bigger and tougher sure, but otherwise they're the same, I think. ";
+            return "Given how everything else has changed, what say you check out a mushroom biome, see what crazy new things you might find there.  ";
         }
         #region Photo Bools
-        public static bool CBat
-        { get { return PhotoManager.PhotoOfNPC[NPCID.GiantBat]; } }
-        public static bool Worm
-        { get { return 
-                    PhotoManager.PhotoOfNPC[NPCID.DiggerHead] ||
-                    PhotoManager.PhotoOfNPC[NPCID.DiggerBody] ||
-                    PhotoManager.PhotoOfNPC[NPCID.DiggerTail]; } }
-        public static bool WallCreeper
-        { get { return PhotoManager.PhotoOfNPC[NPCID.BlackRecluse] || PhotoManager.PhotoOfNPC[NPCID.BlackRecluseWall]; } }
+        public static PhotoManager af = new PhotoManager(NPCID.AnomuraFungus);
+        public static PhotoManager gfb = new PhotoManager(NPCID.GiantFungiBulb);
+        public static PhotoManager ff = new PhotoManager(NPCID.FungoFish);
         #endregion
 
         public override bool CheckPrerequisites(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
@@ -54,29 +48,24 @@ namespace ExpeditionsContent.Quests.Clerk
         public override void CheckConditionCountable(Player player, ref int count, int max)
         {
             count = 0;
-            if (CBat) count++;
-            if (Worm) count++;
-            if (WallCreeper) count++;
+            if (af.checkValid()) count++;
+            if (gfb.checkValid()) count++;
+            if (ff.checkValid()) count++;
         }
 
         public override bool CheckConditions(Player player, ref bool cond1, ref bool cond2, ref bool cond3, bool condCount)
         {
-            cond1 = CBat;
-            cond2 = Worm;
-            cond3 = WallCreeper;
+            cond1 = af.checkValid();
+            cond2 = gfb.checkValid();
+            cond3 = ff.checkValid();
             return cond1 && cond2 && cond3;
         }
 
         public override void PreCompleteExpedition(List<Item> rewards, List<Item> deliveredItems)
         {
-            PhotoManager.ConsumePhoto(NPCID.GiantBat);
-            if (!PhotoManager.ConsumePhoto(NPCID.DiggerHead))
-            {
-                if (!PhotoManager.ConsumePhoto(NPCID.DiggerBody))
-                { PhotoManager.ConsumePhoto(NPCID.DiggerTail); }
-            }
-            if (!PhotoManager.ConsumePhoto(NPCID.BlackRecluseWall))
-            { PhotoManager.ConsumePhoto(NPCID.BlackRecluse); }
+            af.consumePhoto();
+            gfb.consumePhoto();
+            ff.consumePhoto();
 
             // Only reward the coupon once!
             if (expedition.completed)
