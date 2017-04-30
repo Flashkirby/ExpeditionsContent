cb641c45411e4c69118a263763bcc7398b40088e
Flash Kirby <flashkirby@gmail.com>
2017-05-01 00:21:55 +0100

    Change dryad herb quest slightly

--- Quests/MiscPre/DryadHerbs.cs
+++ Quests/MiscPre/DryadHerbs.cs
@@ -29,7 +29,7 @@ namespace ExpeditionsContent.Quests.MiscPre
         public override string Description(bool complete)
         {
             if (complete) return "I'll let you in on a little secret - there's a special staff found in the underground jungle that can be used to harvest seeds from herbs as soon as they are mature - even if they are not in full bloom! It makes herbs like blinkroot much nicer to grow and harvest. ";
-            return "It is important to wait for herbs to bloom before harvesting them. However, sometimes patience isn't always the answer; some herbs will only bloom under certain conditions. For instance, Waterleaf will only bloom in the rain, and Fireblossom will bloom at sunset. Show me your understanding of this, and I will share with you some of my work. ";
+            return "It is important to wait for herbs to bloom before harvesting them. However, sometimes patience isn't always the answer; some herbs will only bloom under certain conditions. For instance, Waterleaf will only bloom in the rain, and Fireblossom will bloom at sunset. Show me some understanding of this, and I will share with you some of my work. ";
         }
     }
 }
